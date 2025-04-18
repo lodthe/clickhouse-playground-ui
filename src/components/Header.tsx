c8ff394cd54eee5d2b77b4244552d32506936bef
Igor Baliuk <baliuk.igor@gmail.com>
2025-04-18 20:06:22 +0100

    Refactor code and reuse the dropdown component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Autocomplete from '@mui/material/Autocomplete';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 import { PlayArrow } from '@mui/icons-material';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import SelectVersion from './SelectVersion';
+import SelectDropdown from './SelectDropdown';
 
 interface HeaderProps {
   tags: string[];
@@ -39,28 +37,34 @@ function Header({
     <Box sx={{ flexGrow: 2 }}>
       <AppBar position="static">
         <Toolbar>
-          <SelectVersion
-            tags={tags}
-            selectedVersion={selectedVersion}
+          <SelectDropdown
+            id="select-clickhouse-version"
+            options={tags}
+            value={selectedVersion}
             onChange={onVersionChange}
-            disabled={requestIsRunning}
+            disabled={requestIsRunning || false}
+            label="ClickHouse Version"
+            sx={{
+              my: 1,
+              px: 1,
+              flexGrow: 1,
+              minWidth: '150px',
+            }}
+            disableDisplay={false}
           />
 
-          <Autocomplete
+          <SelectDropdown
             id="select-output-format"
             options={outputFormats}
             value={selectedFormat}
-            onChange={(event, newValue) => {
-              if (newValue !== null) {
-                const pseudoEvent = {
-                  target: { value: newValue },
-                } as React.ChangeEvent<HTMLInputElement>;
-                onFormatChange(pseudoEvent);
-              }
+            onChange={(newValue) => {
+              const pseudoEvent = {
+                target: { value: newValue },
+              } as React.ChangeEvent<HTMLInputElement>;
+              onFormatChange(pseudoEvent);
             }}
-            disabled={isFormatSelectionDisabled}
-            disableClearable
-            autoSelect
+            disabled={isFormatSelectionDisabled || false}
+            label="Format"
             sx={{
               my: 1,
               px: 1,
@@ -68,22 +72,8 @@ function Header({
               minWidth: '250px',
               flexGrow: 0,
               flexShrink: 0,
-              display: { xs: 'none', sm: 'block' },
-              // Add style to prevent list items from wrapping
-              '& .MuiAutocomplete-listbox .MuiAutocomplete-option': {
-                whiteSpace: 'nowrap',
-              },
             }}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Format"
-                variant="outlined"
-              />
-            )}
-            ListboxProps={{
-              style: { overflowX: 'auto' },
-            }}
+            disableDisplay
           />
 
           <Button
