ac40c6c62f00c5fdb4c6ef5d3a749bab32b72f8a
Igor Baliuk <baliuk.igor@gmail.com>
2025-04-18 21:51:44 +0100

    Allow to change panel width

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter } from 'react-router-dom';
 import WrappedApp from './App';
 import theme from './theme';
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <BrowserRouter>
-      <WrappedApp />
-    </BrowserRouter>
-  </ThemeProvider>,
-  document.querySelector('#root'),
-);
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <WrappedApp />
+        </BrowserRouter>
+      </ThemeProvider>
+    </React.StrictMode>,
+  );
+} else {
+  console.error('Root container missing in index.html');
+}
